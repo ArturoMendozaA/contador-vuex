42e51a9dce03f7a49ad9b95dd026b0e460ff4dbe
Luis <luis.artlinux@gmail.com>
2022-03-25 15:05:36 -0600

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,46 @@
 import { createStore } from 'vuex'
-
+import axios from 'axios'
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: 'blue'
   },
   getters: {
+    contadorCuadrado(state){
+      return state.counter * state.counter;
+    }
   },
   mutations: {
+    increaseCounter(state, randomNumba){
+      console.log('random Number', randomNumba)
+      state.counter += randomNumba
+    },
+    decreaseCounter(state, randomNumba){
+      console.log('random Number', randomNumba)
+      state.counter -= randomNumba
+    },
+    setColorCode(state, textoIn){
+      state.colorCode = textoIn
+    }
   },
   actions: {
+    increaseCounter({ commit }){
+      // console.log('increaseCounter (action)')
+      axios('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new').then(function (respuesta) {
+          // console.log('respuesta', respuesta)
+          commit('increaseCounter', respuesta.data)
+        })
+    },
+    decreaseCounter({commit}){
+      // console.log('decreaseCounter (action )')
+      axios('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new').then(function(respuesta){
+        commit('decreaseCounter', respuesta.data)
+
+      })
+    },
+    setColorCode({ commit }, textoIn){
+      commit('setColorCode', textoIn)
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      :style="{color: $store.state.colorCode}"
+      class="contador">
+      {{ $store.state.counter }}
+    </div>
+    <div class="contadorAlCuadrado">
+      {{$store.state.counter}}
+      <sup>2</sup>=
+      {{$store.getters.contadorAlCuadrado}}
+    </div>
+    <div class="buttons">
+      <button @click="$store.dispatch('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
+    <div>
+      <input
+        v-model="colorCode"
+        type="text"
+        placeholder="Escribe un codigo de color">
+    </div>
+  
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script> 
+ 
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'Home',    
+  computed:{
+    colorCode:{
+      get() {
+        return this.$store.state.colorCode
+      },
+      set(textoIn) {
+        this.$store.dispatch('setColorCode', textoIn)
+      }
+    }
+
   }
 }
 </script>
+<style>
+div{
+  margin-bottom: 10px;
+}
+.contador{
+  font-size: 80px;
+}
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px; /*antes creia otra cosa, pero resulta que es y margin,y  luego el x-margin*/
+
+}
+</style>
